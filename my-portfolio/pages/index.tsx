4931221b415001bb911fd7914716903a5b84fe9d
Kelson Machado Camargo <kelson.m.camargo@hotmail.com>
2023-03-28 17:29:19 -0300

    feat: restart project, day 28/03/2023

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,9 +1,34 @@
-//IMPORTS
-  import  App from './app'
-//IMPORTS
+import styles from '@/styles/Home.module.css'
+
 
 export default function Home() {
   return (
-      <App></App>
+    <div id={styles.body}>
+      <header id={styles.header}>
+       <div id={styles.titleBox}>
+          <h1 id={styles.mainTitle}>PORTFOLIO</h1>
+          <div id={styles.lineTitle}></div>
+        </div>
+
+        <div id={styles.linksNav}>
+          <ul id={styles.listLinks}>
+            <li id={styles.linkNav1}>Home</li>
+            <li id={styles.linkNav2}>Services</li>
+            <li id={styles.linkNav3}>About</li>
+            <li id={styles.linkNav4}>Contact</li>
+          </ul>
+        </div>
+
+        <div id={styles.burgerBox}>
+          <div className={styles.burger} id={styles.burger1}></div>
+          <div className={styles.burger} id={styles.burger2}></div>
+          <div className={styles.burger} id={styles.burger3}></div>
+        </div>
+      </header>
+      <main>
+        
+      </main>
+    </div>
   )
 }
+
