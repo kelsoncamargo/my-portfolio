cc2abff2c754fc90379aba925e9dd66befeba49d
Kelson Machado Camargo <kelson.m.camargo@hotmail.com>
2023-04-13 12:02:39 -0300

    fix: new header componenet add the index, on day 13/04/2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header/header';
 import styles from '@/styles/Home.module.css'
 import React, { useEffect, useState} from 'react'
 
@@ -31,33 +32,7 @@ export default function Home() {
 
   return (
     <div id={styles.body}>
-      <header id={styles.header}>
-       <div id={styles.titleBox}>
-          <h1 id={styles.mainTitle}>PORTFOLIO</h1>
-          <div id={styles.lineTitle}></div>
-        </div>
-
-        <div id={styles.navLinksID} className={verifySizePage()}>
-          <ul id={styles.listLinks}>
-            <li id={styles.linkNav1}>Home</li>
-            <li id={styles.linkNav2}>Services</li>
-            <li id={styles.linkNav3}>About</li>
-            <li id={styles.linkNav4}>Contact</li>
-          </ul>
-        </div>
-
-        <div id={styles.burgerBox} onClick={e => {
-           if(toggle === styles.linksNavNoToggle) {
-              setToggle(styles.listNavToggle)
-            }else if(toggle === styles.listNavToggle) {
-              setToggle(styles.linksNavNoToggle)
-           }
-       }}>
-          <div className={styles.burger} id={styles.burger1}></div>
-          <div className={styles.burger} id={styles.burger2}></div>
-          <div className={styles.burger} id={styles.burger3}></div>
-        </div>
-      </header>
+      <Header/>
       <main>
         
       </main>
--- pages/tmp.tsx
+++
@@ -1,9 +0,0 @@
-import Header from '../components/header/header'
-
-export default function tmp(){
-    return (
-        <div>
-            <Header></Header>
-        </div>
-    )
-}
