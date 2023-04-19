37d535f1ea136890862e31613e87db046768c8a5
Kelson Machado Camargo <kelson.m.camargo@hotmail.com>
2023-04-19 10:39:55 -0300

    fix: resolving the probleam of types 9 | day 19/04/2023

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,7 +3,6 @@
     import styles from './header.module.css'
     import { useState } from 'react'
     import { headerTypes } from './typesHeader'
-    // @ts-ignore
     import { v4 as uuidv4 } from 'uuid';
 //IMPORTS
 
