876ec359a4cf6f07078708d848b41f7c77e5ffee
Kelson Machado Camargo <kelson.m.camargo@hotmail.com>
2023-04-19 09:27:35 -0300

    fix: name components Alert in Header and Index - 4 | 19/04/2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 //IMPORTS
   import Header from '@/components/Header/header'
   import styles from '../styles/Home.module.css'
-  import {Links} from './types'
+  import {Links} from '../types/types'
 //IMPORTS
 
 export default function Home() {  
